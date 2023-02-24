162e4db774839dce98d51d60618f788acc4edf62
“ehsan-elgendi” <ehsan.elgendi@gmail.com>
2023-02-23 20:50:21 -0500

    add mocks data,fix selected day style bug,add waitForElement to Application test

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,12 +4,14 @@ import DayListItem from "./DayListItem";
 export default function DayList (props) {
 
   const days = props.days.map((day) => {
+    console.log('day:', day.name);
+    console.log('value:', props.value);
     return (
       <DayListItem
         key={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.value}
+        selected={day.name === props.day}
         setDay={props.onChange}
       />
     );
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,11 @@
 import React from "react";
-
-import "components/DayListItem.scss";
 import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots
+    "day-list__item--full": props.spots === 0
   });
 
   const formatSpots = () => {
@@ -16,7 +15,7 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
